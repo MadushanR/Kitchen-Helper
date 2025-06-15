0d9b187b18e16807298db3aecc05e4dba5c8d32e
MadushanR <madushanrajendran@gmail.com>
2025-06-14 21:47:32 -0400

    frontend done

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -3,5 +3,5 @@ from .models import Item
 from .serializers import ItemSerializer
 
 class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all().order_by('-created_at')
+    queryset         = Item.objects.all().order_by('-created_at')
     serializer_class = ItemSerializer
